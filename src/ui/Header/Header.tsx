f1923ee07288b2df9bcbe354825d542b53a8fcf3
endifai <dimasik.uva@yandex.ru>
2021-08-22 12:19:49 +0400

    add header

--- src/ui/Header/Header.tsx
+++ src/ui/Header/Header.tsx
@@ -2,4 +2,17 @@ import './Header.scss'
 
 import { ReactElement } from 'react'
 
-export const Header = (): ReactElement => <header></header>
+export const Header = (): ReactElement => (
+  <header>
+    <img src="./images/logo.png" className="logo" draggable={false} />
+
+    <div>
+      <a href="/" className="nav-link">
+        Главная
+      </a>
+      <a href="/favorites" className="nav-link">
+        Избранное
+      </a>
+    </div>
+  </header>
+)
