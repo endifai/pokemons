c31f5ee5f547aded134aa79fa0df4771aa56c89a
endifai <dimasik.uva@yandex.ru>
2021-09-25 18:25:59 +0400

    feat: update colors for pokemon types

--- src/ui/pokemon-types/pokemon-types.tsx
+++ src/ui/pokemon-types/pokemon-types.tsx
@@ -3,15 +3,25 @@ import './pokemon-types.scss'
 import { ReactElement } from 'react'
 
 import { PokemonItem } from '../../types'
+import { getColorByBgColor, typeColors } from './type-colors'
 
 type Props = Pick<PokemonItem, 'types'>
 
 export const PokemonTypes = ({ types }: Props): ReactElement => (
   <div className="pokemon__types">
-    {types.map(({ type: { name } }) => (
-      <span key={name} className="pokemon__types__type">
-        {name}
-      </span>
-    ))}
+    {types.map(({ type: { name } }) => {
+      const backgroundColor = typeColors[name as keyof typeof typeColors]
+      const color = getColorByBgColor(backgroundColor)
+
+      return (
+        <span
+          key={name}
+          className="pokemon__types__type"
+          style={{ backgroundColor, color }}
+        >
+          {name}
+        </span>
+      )
+    })}
   </div>
 )
---
+++ src/ui/pokemon-types/type-colors.ts
@@ -0,0 +1,28 @@
+export const typeColors = {
+  normal: '#A8A77A',
+  fire: '#EE8130',
+  water: '#6390F0',
+  electric: '#F7D02C',
+  grass: '#7AC74C',
+  ice: '#96D9D6',
+  fighting: '#C22E28',
+  poison: '#A33EA1',
+  ground: '#E2BF65',
+  flying: '#A98FF3',
+  psychic: '#F95587',
+  bug: '#A6B91A',
+  rock: '#B6A136',
+  ghost: '#735797',
+  dragon: '#6F35FC',
+  dark: '#705746',
+  steel: '#B7B7CE',
+  fairy: '#D685AD',
+}
+
+/**
+ * Функция определяющая цвет для текста в зависимости от цвета заднего фона
+ *
+ * @param bgColor - цвет background
+ */
+export const getColorByBgColor = (bgColor: string): string =>
+  parseInt(bgColor.replace('#', ''), 16) > 0xffffff / 2 ? '#000000' : '#ffffff'
