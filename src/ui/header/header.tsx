9861f57c7b1d159f96ca222475e466458d3123e7
endifai <dimasik.uva@yandex.ru>
2021-09-25 18:43:17 +0400

    feat: стили для активного страницы в хедере

--- src/ui/header/header.tsx
+++ src/ui/header/header.tsx
@@ -1,21 +1,51 @@
 import './header.scss'
 
+import classNames from 'classnames'
 import { ReactElement } from 'react'
-import { Link } from 'react-router-dom'
+import { Link, useHistory, useLocation } from 'react-router-dom'
 
 import { RoutesEnum } from '../../enums/routes.enum'
 
-export const Header = (): ReactElement => (
-  <header>
-    <img src="./images/logo.png" className="logo" draggable={false} />
+const links = [
+  {
+    path: RoutesEnum.MAIN,
+    label: 'Главная',
+  },
+  {
+    path: RoutesEnum.FAVORITES,
+    label: 'Избранное',
+  },
+]
 
-    <div>
-      <Link to={RoutesEnum.MAIN} className="nav-link">
-        Главная
-      </Link>
-      <Link to={RoutesEnum.FAVORITES} className="nav-link">
-        Избранное
-      </Link>
-    </div>
-  </header>
-)
+export const Header = (): ReactElement => {
+  const history = useHistory()
+  const { pathname } = useLocation()
+
+  const handleLogoClick = () => history.push(RoutesEnum.MAIN)
+
+  return (
+    <header>
+      <img
+        src="./images/logo.png"
+        className="logo"
+        draggable={false}
+        onClick={handleLogoClick}
+      />
+
+      <div>
+        {links.map(({ path, label }) => (
+          <Link
+            key={path}
+            to={path}
+            className={classNames(
+              'nav-link',
+              pathname === path && 'active-link',
+            )}
+          >
+            {label}
+          </Link>
+        ))}
+      </div>
+    </header>
+  )
+}
